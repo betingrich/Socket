229c27f00a53b318a8f726d9207ad09701d8c7d6
marisel <140407389+betingrich@users.noreply.github.com>
2025-04-13 00:24:14 +0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,12 +1,5 @@
 $(function() {
   const FADE_TIME = 150;
-  const TYPING_TIMER_LENGTH = 400;
-  const COLORS = [
-    '#e21400', '#91580f', '#f8a700', '#f78b00',
-    '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
-    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
-  ];
-
   const $window = $(window);
   const $usernameInput = $('.usernameInput');
   const $messages = $('.messages');
@@ -14,105 +7,66 @@ $(function() {
   const $loginPage = $('.login.page');
   const $chatPage = $('.chat.page');
 
-  const socket = io();
+  // Connect to server
+  const socket = io('http://localhost:3000', {
+    reconnectionAttempts: 5
+  });
+
+  // Debugging events
+  socket.on('connect', () => console.log('Connected to server'));
+  socket.on('connect_error', (err) => console.log('Connection error:', err));
+  socket.on('disconnect', () => console.log('Disconnected'));
 
   let username = '';
   let connected = false;
-  let typing = false;
-  let lastTypingTime;
-  let $currentInput = $usernameInput.focus();
-
-  const addParticipantsMessage = (data) => {
-    const message = data.numUsers === 1 
-      ? "there's 1 participant" 
-      : `there are ${data.numUsers} participants`;
-    log(message);
-  };
 
   const setUsername = () => {
     username = cleanInput($usernameInput.val().trim());
+    console.log('Setting username:', username); // Debug log
 
     if (username) {
       $loginPage.removeClass('active');
       $chatPage.addClass('active');
-      $currentInput = $inputMessage.focus();
+      $inputMessage.focus();
       socket.emit('add user', username);
     }
-  };
-
-  const sendMessage = () => {
-    const message = cleanInput($inputMessage.val());
-    if (message && connected) {
-      $inputMessage.val('');
-      addChatMessage({ username, message });
-      socket.emit('new message', message);
-    }
-  };
-
-  const log = (message, options) => {
-    const $el = $('<li>').addClass('log').text(message);
-    addMessageElement($el, options);
-  };
-
-  const addChatMessage = (data, options = {}) => {
-    const $typingMessages = getTypingMessages(data);
-    if ($typingMessages.length !== 0) {
-      options.fade = false;
-      $typingMessages.remove();
-    }
-
-    const $usernameDiv = $('<span class="username"/>')
-      .text(data.username)
-      .css('color', getUsernameColor(data.username));
-    const $messageBodyDiv = $('<span class="messageBody">').text(data.message);
-
-    const $messageDiv = $('<li class="message"/>')
-      .data('username', data.username)
-      .append($usernameDiv, $messageBodyDiv);
-
-    addMessageElement($messageDiv, options);
-  };
-
-  const addMessageElement = (el, options) => {
-    const $el = $(el);
-    if (!options.fade) {
-      $el.hide().fadeIn(FADE_TIME);
-    }
-    $messages.append($el);
-    $messages[0].scrollTop = $messages[0].scrollHeight;
   };
 
   const cleanInput = (input) => {
     return $('<div/>').text(input).html();
   };
 
+  // Keyboard events
   $window.keydown(event => {
-    if (event.which === 13) {
-      if (username) {
-        sendMessage();
-      } else {
+    if (event.which === 13) { // Enter key
+      if (!username) {
         setUsername();
       }
     }
   });
 
+  // Socket events
   socket.on('login', (data) => {
+    console.log('Login successful', data); // Debug log
     connected = true;
-    log('Welcome to Socket.IO Chat');
-    addParticipantsMessage(data);
+    addMessage('Welcome to the chat');
   });
 
   socket.on('new message', (data) => {
-    addChatMessage(data);
+    addMessage(`${data.username}: ${data.message}`);
   });
 
   socket.on('user joined', (data) => {
-    log(`${data.username} joined`);
-    addParticipantsMessage(data);
+    addMessage(`${data.username} joined the chat`);
   });
 
   socket.on('user left', (data) => {
-    log(`${data.username} left`);
-    addParticipantsMessage(data);
+    addMessage(`${data.username} left the chat`);
   });
+
+  function addMessage(message) {
+    const $message = $('<li>').text(message);
+    $messages.append($message);
+    $messages[0].scrollTop = $messages[0].scrollHeight;
+  }
 });
